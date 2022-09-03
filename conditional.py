95469b5df40f3b6c96b34782384cb1f9f2f3b050
Pratik Balaso Patil <patilpratik269@gmail.com>
2022-09-03 17:38:48 +0530

    after deletion

--- conditional.py
+++
@@ -1,9 +0,0 @@
-a = int(input('Enter number: '))
-b = int(input('Enter number: '))
-
-if a>b:
-    print(f'{a} is greater than {b}')
-elif a<b:
-    print(f'{a} is less than {b}')
-else:
-    print(f'{a} is equal to {b}')

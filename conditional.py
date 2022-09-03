38c54441c8c81a3cebc9b6ef461593f7bc294889
Pratik Balaso Patil <patilpratik269@gmail.com>
2022-09-03 17:09:03 +0530

    condional expressions added

---
+++ conditional.py
@@ -0,0 +1,9 @@
+a = int(input('Enter number: '))
+b = int(input('Enter number: '))
+
+if a>b:
+    print(f'{a} is greater than {b}')
+elif a<b:
+    print(f'{a} is less than {b}')
+else:
+    print(f'{a} is equal to {b}')
